a08e12765acc9cca5e47524e59ca989a1f4e793c
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-07 10:04:50 +0800

    herokuhelp

---
+++ alias.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+source ~/.alias/ag/ag.alias
+source ~/.alias/blueutil/blueutil.alias
+source ~/.alias/date/date.alias
+source ~/.alias/df/df.alias
+source ~/.alias/dotnet/dotnet.alias
+source ~/.alias/docker/docker.alias
+source ~/.alias/find/find.alias
+source ~/.alias/git/git.alias
+source ~/.alias/history/history.alias
+source ~/.alias/ls/ls.alias
+source ~/.alias/python/python.alias
+source ~/.alias/react/react-native.alias
+source ~/.alias/sed/sed.alias
+source ~/.alias/suffix.alias
+source ~/.alias/tar/tar.alias
+source ~/.alias/vagrant/vagrant.alias
+source ~/.alias/vim/vim.alias
+source ~/.alias/heroku/heroku.alias
